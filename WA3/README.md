60641297352cee2a5d643244c376135a0c163a7f
Brett <baxelrad>
2019-10-20 18:50:50 -0400

    Final commit.

---
+++ WA3/README.md
@@ -0,0 +1,11 @@
+#  Brett Axelrad, Weekend Assignment 3
+
+## How app works:
+
+### Screenshots in 'Run Screenshots' folder.
+
+- Pulls from API and displays thumbnails, albumId, id, and title (Tablescreen.png).
+- Random colors are generated at program run, and colors are assigned to albumId's.
+- Can click on a cell to see a the whole image (Imagescreen.png).
+- Can click back button to go back, or swipe up.
+
--- WA3/View/ImageViewController.swift
+++ WA3/View/ImageViewController.swift
@@ -6,13 +6,25 @@ class ImageViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     
     var image: UIImage?
+    
+    lazy var swipeGesture: UISwipeGestureRecognizer = {
+        let swipe = UISwipeGestureRecognizer(target: self,
+                                             action: #selector(swipeAction))
+        swipe.direction = .down
+        return swipe
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        imageView.addGestureRecognizer(swipeGesture)
+        imageView.isUserInteractionEnabled = true
+
     }
     
-
+    @objc func swipeAction() {
+        dismiss(animated: true, completion: nil)
+    }
   
 
 }
